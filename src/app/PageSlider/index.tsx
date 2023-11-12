1a04f32dd56cd54c90fcfc971db23b96e659a99f
AayushOjha <ayush2ojha22ayush@gmail.com>
2023-11-12 17:33:29 +0530

    added diwali content with ui changes and fixed route

--- src/app/PageSlider/index.tsx
+++ src/app/PageSlider/index.tsx
@@ -2,8 +2,9 @@
 
 import { BgColors } from "@/constant";
 import { IJoke } from "@/interfaces";
+import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
-// import Markdown from "react-markdown";
+import Markdown from "react-markdown";
 
 interface IProps {
   jokes: IJoke[];
@@ -33,26 +34,41 @@ function PageSlider({ jokes }: IProps) {
   }, []);
 
   return (
-    <div
-      ref={scrollableDivRef}
-      className="w-screen h-screen overflow-auto snap-x snap-mandatory scroll-smooth flex no-sidebar-visibility"
-    >
-      {jokes.map((joke, index) => {
-        const bgColor = BgColors[index % 10];
-        return (
-          <div
-            key={index}
-            id={joke.slug}
-            style={{ backgroundColor: bgColor }}
-            className="w-full h-full pt-20 p-4 flex-shrink-0 snap-start leading-8 text-white overflow-hidden"
-          >
-            <div className="w-full max-w-xl m-  auto">
-              <p>{joke.content}</p>
+    <>
+      <div
+        ref={scrollableDivRef}
+        className="w-screen h-screen overflow-auto snap-x snap-mandatory scroll-smooth flex no-sidebar-visibility"
+      >
+        {jokes.map((joke, index) => {
+          const bgColor = BgColors[index % 10];
+          return (
+            <div
+              key={index}
+              id={joke.slug}
+              style={{ backgroundColor: bgColor }}
+              className="w-full h-full pt-48 p-4 flex-shrink-0 snap-start leading-8 text-white overflow-hidden"
+            >
+              <div className="w-full max-w-xl m-auto">
+                <Markdown>{joke.content}</Markdown>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+
+      {scrollPosition < 600 ? (
+        <div className="absolute z-10 bottom-10 w-40 left-1/2">
+          <img
+            src={
+              "https://res.cloudinary.com/dca1rzsyb/image/upload/v1699790486/mohannic/swipe_ucsc80.gif"
+            }
+            alt="navigation helper"
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
