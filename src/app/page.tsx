5c192f40432671a1d7903cc82b42c959513cbdd1
AayushOjha <ayush2ojha22ayush@gmail.com>
2023-11-12 18:29:36 +0530

    linked page to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
@@ -6,10 +7,12 @@ export default function Home() {
       <div>
         <h1 className="text-white font-semibold text-3xl mb-4">COMING SOON</h1>
         <p className="text-white font-semibold text-5xl mb-8">MOHANNIC.COM</p>
-        <button className="text-white w-full bg-transparent px-6 py-2 rounded-md border border-white flex items-center justify-center">
-          Continue to App
-          <FiArrowRight className="ml-2" />
-        </button>
+        <Link href={`https://mohannic.com/diwali-jokes`}>
+          <button className="text-white w-full bg-transparent px-6 py-2 rounded-md border border-white flex items-center justify-center">
+            Continue to App
+            <FiArrowRight className="ml-2" />
+          </button>
+        </Link>
       </div>
     </div>
   );
