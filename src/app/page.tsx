1b8edb30af6ddeba4051ccdd2d1ff584f37eb6e7
AayushOjha <ayush2ojha22ayush@gmail.com>
2023-11-18 08:35:44 +0530

    updated home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,12 @@ export default function Home() {
   return (
     <div className="bg-[#353b48] h-screen flex justify-center items-center">
       <div>
-        <h1 className="text-white font-semibold text-3xl mb-4">COMING SOON</h1>
-        <p className="text-white font-semibold text-5xl mb-8">MOHANNIC.COM</p>
+        <h1 className="text-white font-semibold text-xl lg:text-3xl mb-1">
+          COMING SOON
+        </h1>
+        <p className="text-white font-semibold text-2xl lg:text-5xl mb-2">
+          MOHANNIC.COM
+        </p>
         <Link href={`https://mohannic.com/diwali-jokes`}>
           <button className="text-white w-full bg-transparent px-6 py-2 rounded-md border border-white flex items-center justify-center">
             Continue to App
