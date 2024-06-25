230ec0603c693b5712977024151eec5b0e736901
Ayush <ayush2ojha22ayush@gmail.com>
2024-06-25 19:55:31 +0530

    added GTM tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager  */}
+          <Script id="gtm">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true; j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M9P6LV2K');`}
+          </Script>
+        {/* End Google Tag Manager  */}
+      </head>
       <body className={inter.className}>
+        
+        {/* Google Tag Manager (noscript)  */}
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M9P6LV2K"
+        // @ts-ignore
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript)  */}
+
         <div className="_nextxxx">{children}</div>
 
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-GNBEQ7GYCW" />
